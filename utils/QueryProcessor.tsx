980c4cd0f0b0474324e5452643998851f8f07b72
Andrew <andrewwang12345@gmail.com>
2024-10-10 10:09:25 -0400

    multiply

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -35,6 +35,15 @@ export default function QueryProcessor(query: string): string {
       return (x+y).toString();
     }
   }
+
+  if (query.includes("multiplied")) {
+    const addMatch = query.match(/What is (\d+) multiplied by (\d+)/);
+    if (addMatch) {
+      const x: number = parseInt(addMatch[1]);
+      const y: number = parseInt(addMatch[2]);
+      return (x*y).toString();
+    }
+  }
   
 
   return "";
