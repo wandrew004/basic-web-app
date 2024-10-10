d020ee177e6449db6752bbb799f368be3dd8963b
Andrew <andrewwang12345@gmail.com>
2024-10-10 10:32:51 -0400

    addd

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -28,11 +28,12 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.includes("plus")) {
-    const addMatch = query.match(/What is (\d+) plus (\d+)/);
+    const addMatch = query.match(/What is (\d+) plus (\d+) plus (\d+)/);
     if (addMatch) {
       const x: number = parseInt(addMatch[1]);
       const y: number = parseInt(addMatch[2]);
-      return (x+y).toString();
+      const z: number = parseInt(addMatch[3]);
+      return (x+y+z).toString();
     }
   }
 
