0293f017be09a49cdfe27b62dd30fa0839f8287c
Andrew <andrewwang12345@gmail.com>
2024-10-10 10:12:52 -0400

    square and cube

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -44,6 +44,15 @@ export default function QueryProcessor(query: string): string {
       return (x*y).toString();
     }
   }
+
+  if (query.includes("square")) {
+    const addMatch = query.match(/Which of the following numbers is both a square and a cube: (\d+), (\d+), (\d+), (\d+), (\d+), (\d+), (\d+)/)
+    if (addMatch) {
+      const numArray = [parseInt(addMatch[1]), parseInt(addMatch[2]), parseInt(addMatch[3]), parseInt(addMatch[4]), parseInt(addMatch[5]), parseInt(addMatch[6]), parseInt(addMatch[7])]
+      const squareAndCube = numArray.filter(num => Math.sqrt(num) % 1 === 0 && Math.cbrt(num) % 1 === 0)
+      return squareAndCube.toString();
+    }
+  }
   
 
   return "";
